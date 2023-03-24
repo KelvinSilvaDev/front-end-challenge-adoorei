cee5f998087810266565be2244d1559147ec7cd7
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2023-03-24 15:50:56 -0300

    Feat: Banner for home creation

---
+++ src/components/Banner.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="relative">
+    <img
+      :src="bannerImageUrl"
+      alt="banner image"
+      class="w-full h-auto object-cover"
+    />
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div
+      class="absolute inset-0 flex flex-col items-center justify-center px-4 md:px-8 text-center text-white"
+    >
+      <h1 class="text-4xl font-bold mb-4">{{ title }}</h1>
+      <p class="text-lg mb-8">{{ subtitle }}</p>
+      <a href="#product-list" class="inline-block">
+        <button
+          class="bg-white text-black font-bold py-3 px-6 rounded-full transition-all duration-300 hover:bg-black hover:text-white shadow-lg"
+        >
+          <span>Comprar</span>
+          <i class="fas fa-shopping-cart ml-2"></i>
+        </button>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+    buttonLabel: {
+      type: String,
+      required: true,
+    },
+    bannerImageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+img {
+  height: 400px;
+}
+@media (min-width: 768px) {
+  img {
+    height: 600px;
+  }
+}
+button {
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s ease;
+}
+button:hover {
+  background-color: black;
+  color: white;
+}
+button:hover i {
+  color: white;
+}
+i {
+  font-size: 1.2rem;
+  vertical-align: middle;
+  transition: color 0.3s ease;
+}
+a:focus {
+  outline: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,30 @@
 <template>
+  <Banner
+    :bannerImageUrl="bannerImageUrl"
+    title="Descubra a melhor seleção de produtos para você"
+    subtitle="Na nossa loja virtual, oferecemos uma ampla seleção de produtos de qualidade superior, que atendem às suas necessidades e preferências. Navegue pela nossa variedade de produtos, encontre o que você precisa e receba na comodidade da sua casa. Não perca mais tempo e adquira agora mesmo os produtos que vão facilitar o seu dia a dia e torná-lo mais prático e agradável."
+    buttonLabel="Comprar"
+  />
   <ProductList :products="products" />
 </template>
 <!-- @add-to-cart="addToCart" -->
 <script>
 import ProductList from "../components/ProductList.vue";
+import Banner from "../components/Banner.vue";
 import Vuex, { mapActions, mapMutations } from "vuex";
 import api from "../services/api";
 
 export default {
   name: "Products",
   components: {
+    Banner,
     ProductList,
   },
   data() {
     return {
       products: [],
+      bannerImageUrl:
+        "https://images.unsplash.com/photo-1580828343064-fde4fc206bc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
     };
   },
   mounted() {
